470cf0070420ab2428a3ea03ceaa3cb7075650ac
micitabot-max <micitabot@gmail.com>
2026-05-20 12:41:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const http = require('http');
 const url = require('url');
+const https = require('https');
+
+const MAKE_URL = 'https://hook.eu1.make.com/l1llqubssf4eaqqr42cgs4rgccjghf1g';
+const VERIFY_TOKEN = 'micitabot123';
 
 const server = http.createServer((req, res) => {
-  const query = url.parse(req.url, true).query;
-  if (query['hub.verify_token'] === 'micitabot123') {
-    res.writeHead(200);
-    res.end(query['hub.challenge']);
-  } else {
-    res.writeHead(403);
-    res.end('Forbidden');
+  if (req.method === 'GET') {
+    const query = url.parse(req.url, true).query;
+    if (query['hub.verify_token'] === VERIFY_TOKEN) {
+      res.writeHead(200);
+      res.end(query['hub.challenge']);
+    } else {
+      res.writeHead(403);
+      res.end('Forbidden');
+    }
+  } else if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => { body += chunk; });
+    req.on('end', () => {
+      const makeReq = https.request(MAKE_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      makeReq.write(body);
+      makeReq.end();
+      res.writeHead(200);
+      res.end('OK');
+    });
   }
 });
 
